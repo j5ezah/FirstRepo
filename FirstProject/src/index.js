44432d49c3f6d22450399facd14b2671d07e35ac
Ezah <ikechukwue39@gmail.com>
2023-11-16 21:44:22 +0100

    assignments

--- FirstProject/src/index.js
+++ FirstProject/src/index.js
@@ -1,1 +1,85 @@
-console.log("Hey there this is first trial..")
+// console.log("Hey there this is first trial..")
+// console.log("hello world")
+
+// variables
+// let const var
+// let customString = "Summer Holiday"
+
+// console.log(customString.replace("Summer Holiday","jaceBeauty"))
+// console.log(customString.startsWith("n",3))
+// console.log(customString[13])
+/**
+ *1. create a variable.
+  2. save a random string value in the variable.
+ * 3. Get the total length of the sub-strings stored in the variable.
+   4. Get the value at the last index position of the string.
+   5. Replace the first letter of the string value with the word "This"
+   6. Check if the string starts with the word "this"
+
+   Note: all insdtructions must be followed to book!
+
+ * 
+ * 
+ */
+// let hummerBoy = "creative lines"
+// console.log(hummerBoy.length)
+// console.log(hummerBoy[13])
+// console.log(hummerBoy.replace("c","THis"))
+// console.log(hummerBoy.startsWith("this"))
+// Arrays in javaScript
+
+// let myArray = [5,"second value", 6]
+
+// console.log(myArray.pop())
+// console.log(myArray)
+
+/**
+ * assignments on string 
+ * 1. create a variable
+ * 2. save a string with the value "Assignment string"
+ * 3. convert all the string to upperCase
+ * 4. Replace the "Assignment" in the string with "Test"
+ * 5. 
+ * 
+ * 
+ * Assginment on Array
+ * 
+ * 1. create a variable
+ * 2. save an array object in the variable
+ * 3. log the length of the array in the console.
+ * 4. push the value "beauty" into the array
+ */
+let joyPack = "winterbed"
+
+let winePress = "Assignment string"
+console.log(winePress.toUpperCase())
+console.log(winePress.replace("Assignment","test"))
+
+let table = "sugarpin"
+console.log(table.length)
+console.log(table.replace())
+// More assignments on strings and array
+
+/** For string
+ * 1. create a variable
+ * 2. store the string value: "Happy coding in javascript" in the variable.
+ * 3. replace "Happy coding" with "Doing coding"
+ * 4. convert all the letters to to lower case
+ * 
+ * 
+ * For Arrays
+ * 
+ * 1. create an array in a variable.
+ * 2. store three items in the array, namely: "Jace", 25, "happiness"
+ * 3. push the word: "mood" into the array.
+*/
+let bankPay = "Happy coding in java script"
+console.log(bankPay.replace("Happy coding","Doing coding"))
+console.log(bankPay.toLowerCase)
+
+let fruit = ["Jace","25","happiness"];
+fruit.push('mood');
+console.log(fruit);
+const numbers = [1,2,3,];
+numbers.push(4);
+console.log(numbers);
