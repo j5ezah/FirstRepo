0a603c0d5020ae255f8eaea54dabfdeb8115eb41
Ezah <ikechukwue39@gmail.com>
2023-12-07 20:43:48 +0100

    worked on logical operations

--- FirstProject/src/index.js
+++ FirstProject/src/index.js
@@ -89,21 +89,18 @@
  * 2. Check if the string starts with "How", and store the result in a variable
  * 3. Check if the string ends with "Jace", and store the result in a variable
  * 4. If the string ends with "Jace", log it into the console.
- */ 
-let sandra = 'Hey Jace';
-let startWithString = sandra.startsWith("How")
-let endWithString = sandra.endsWith("Jace")
-if(endWithString){
-  console.log(sandra)
+ *
+let boneTin = "Hey Jace"
+let startWithString = train.startWithString("How")
+let endWithString = bakeEnds.endWithString("Jace")
+
+if(endWithJace){
+  console.log(jac.endWithString("Jace"));
 }
 
 let computer = 'colourful';
 console.log(computer)
-
-
-
-
-
+*/
 
 // let bigi = 'Hey Jace'
 // let startResult = bigi.startsWith('How')
@@ -160,8 +157,8 @@ console.log(computer)
  * 1. Comparison operators:
  *    a. >
  *    b. < 
- *    c. ==
- *    d. ===
+ *    c. == (Checks for equality by values of the objects alone)
+ *    d. === (Checks for equality by values and type of objects)
  *    e. !=
  *    f. >=
  *    g. <=
@@ -252,9 +249,243 @@ console.log(john - ruddy)
  * 
  * 
  */
-let hour = 12;
-let minute = 30;
-console.log(hour == 12 && minute == 30)
+let hour = false;
+let minute = false;
+let otherOne = true
+let otherTwo = true
 
 
+// console.log(hour || minute)
+console.log(minute && otherOne)
+console.log(otherTwo && hour || otherOne && minute || hour)
+console.log(otherOne || otherTwo || minute && minute && hour && minute || hour)
+console.log(minute || hour || hour && otherTwo && minute && otherOne || otherOne)
+console.log(otherOne && otherTwo || minute && hour || hour)
+console.log(otherOne && minute || minute && hour || hour)
 // hour = hour + minute
+
+
+//LOGICAL "NOT" OPERATOR EXAMPLES!
+// let glass =  true; television  = false;
+// console.log(!glass);
+// console.log(!television);
+
+
+// assigment operators
+let a = 5; let b = 2; let c = 'hello'
+// comparison operators
+// console.log(a == 5);
+// console.log(b == '2');
+// console.log (c == 'hello');
+// not equal to operator
+
+// let sun = 3, moon = 'hello';
+// console.log(sun != 2);
+// console.log(moon != 'hello');
+
+//strict equal to operator
+
+// let back = 2
+// console.log(back === 2);
+// console.log(back === '2')
+
+// strict not equal to operator
+
+// let adam = 2, eve = 'hello'
+// console.log(adam !== '2');
+// console.log(adam !== '2');
+// console.log(eve !== 'hello');
+
+// greater than operator
+
+// let brack = 3;
+// console.log(brack > 2);
+
+// greater or equal to
+// let bottle = 3;
+// console.log(bottle >= 3);
+
+// less than operators
+// let house = 3,  curtain = 2;
+// console.log(house < 2);
+// console.log(curtain < 3);
+
+// less than or equal to operator
+// let chair = 3;
+// console.log(chair <= 3);
+// console.log(chair <= 2);
+// The concept of "Truthy" and "Falsy" in programming.
+/**
+ * The main goal of this concept is to help identify which expression evaluates to either "true" or "false"
+ * in a program (written by you or someone else -- javascript for now.)
+ * 
+ * On the basic level, "truthy" and "falsy" checks are mostly against objects created by the data types of our programming language
+ */
+
+
+// let myCustomArray = []
+/**
+ * Conditional Statements
+ *-----------------------
+  Conditional statements help perform operations in our programs based on whether certain conditions are met.
+
+
+  Types:
+  1. if ()
+  2. else if ()
+  3. else ()
+
+Example:
+Consider a situation where you have to go to a firend's party on a Saturday.
+Now, the Saturday arrives, but the rain started falling on this day.
+For you not to miss your friends party, there are 2 options available:
+1. Your friend would send a car to pick you up
+2. Your Mechanic would bring your previously faulty car which he has repaired
+Tasks:
+   a. check if your friend sends you a car to pick you up. If he does, go to the party. Or
+   b. check if your mechanic brings your car. If he does, go to the party.
+   c. if none of those were true, don't go to the party.
+ */
+
+
+const PARTY_OF_FRIEND = true
+const RAIN_FALLING = true
+
+let friendCarAvailable = false
+let myCarRepaired = true
+
+
+if(friendCarAvailable){
+  console.log(`I'm going to my friend's party!`)
+}
+else if(myCarRepaired){
+  console.log(`I'm going to my friend's party!`)
+}
+else{
+  console.log(`I'm not going to the party because there is no car available!`)
+}
+
+/**
+ * Assignment
+ * ------------------
+ * Problem Statement:
+ * 
+ * You wake up on one morning feeling very hungry.
+ * There are no food at home to eat, you can't make a transfer to a vendor, and you have no ATM card; hence, you only have 3 options to survive that morning:
+ *    a. You should visit your reliable friend to eat at his home, ONLY if he is at home.
+ *    b. You should find an ongoing party in your area, dress up and attend for the food shared.
+ *    c. You should wait till your wife arrives from her travel with groceries to make some food.
+ * 
+ * Tasks:
+ *    a. check whether your friends is around. If so, log "I will eat at my friend's house" into the console.
+ *    b. check if there is an ongoing party in your area. If so, log "I will eat at the party" into the console.
+ *    c. confirm if your wife is still coming back from her travel on that same day she mentioned. If so, log "I will eat at my home" into the console.
+ *    d. if none of the conditions above are true, log "This one na die oh!" into the console.
+ */  
+
+//assignment 1
+// let friendIsHome = false
+// let thereIsPartyToday = false
+// let wifeArrivalToday = false
+
+// if(friendIsHome){
+//   console.log(`I will eat at my friend's house`)
+// }
+// else if(thereIsPartyToday){
+//   console.log(`I will eat at the party`)
+// }
+// else if(wifeArrivalToday){
+//   console.log(`I will eat at my home`)
+// }
+/**
+ * else{
+ *  console.log("This one na die o!")
+ * }
+ * 
+ */
+
+// assignment 2
+
+let friendIsHome = false
+let thereIsPartyToday = false
+let wifeArrivalToday = true
+ 
+if(friendIsHome){
+  console.log(`I will eat at my friends's house`)
+}
+else if(thereIsPartyToday){
+  console.log(`I will eat at the party`)
+}
+else if(wifeArrivalToday){
+  console.log(`I will eat at home`)
+}
+else{
+  console.log(`This one na die oh!`)
+}
+// array
+
+//or Arrays
+
+//  * 1. create an array in a variable.
+//  * 2. store three items in the array, namely: "Jace", 25, "happiness"
+//  * 3. push the word: "mood" into the array.
+// *//
+// let myPower  = ['jace', 25, 'happiness']
+// myPower.push("mood", "anotherPush", "whateverPush")
+// console.log(myPower)
+
+/**
+ * Assignment (3/12/2023)
+ * ----------
+ * let father = true
+ * let mother = true
+ * let children = false
+ * let family = father && mother && children
+ * -------------------------------------------
+ * let ceoIsAvailable = true
+ * let workersAvailable = false
+ * let companyBuilding = true
+ * --------------------------
+ * 
+ * Task1
+ * -----
+ * 1. check if father and mother are available. If so, log "family is almost complete" into the console.
+ * 2. otherwise, check if either only father or mother is available. If so, log "no family yet" into the console.
+ * 3. otherwise, log "Either family is empty or the full family available" into the console.
+ * 
+ * 
+ * 
+ * Tast2
+ * -----
+ * 
+ */
+// let father = true
+// let mother = true
+// let children = false
+// let family = father && mother && children
+
+// if(father && mother){
+//   console.log("family is almost complete")
+// }
+// else if(father || mother){
+//   console.log("no family yet")
+// }
+// else{
+//   console.log(" Either family is empty the full family available")
+// }
+
+let perf = 9
+if(perf < 10 || perf > 18){
+  console.log('The office is closed.');
+}
+
+let hours = 12
+if(hours < 10 || hours > 18){
+  console.log("The office is closed.");
+}
+
+let hourse = 12 
+let minutes = 30
+if(hourse == 12 && minutes == 30){
+  console.log('The time is 12:30');
+}
